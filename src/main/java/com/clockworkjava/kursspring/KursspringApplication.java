766bfed11c5ab146c8aafbb645ef9fdbd2b75f2b
kt1313 <tomaklim@wp.pl>
2021-08-16 06:49:13 +0200

    Lekcja 10 - Wstrzykiwanie Zaleznosci

--- src/main/java/com/clockworkjava/kursspring/KursspringApplication.java
+++ src/main/java/com/clockworkjava/kursspring/KursspringApplication.java
@@ -1,13 +1,15 @@
 package com.clockworkjava.kursspring;
 
+import com.clockworkjava.kursspring.domain.Castle;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KursspringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KursspringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(KursspringApplication.class, args);
+    }
 }
