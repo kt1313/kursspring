4ed779825b13b5187135a7c6187037f2e2274ce0
kt1313 <tomaklim@wp.pl>
2021-08-19 04:11:52 +0200

    Lekcja 21 - ZaawansowaneDI-Wstzrykiwanie kolekcji

--- src/main/java/com/clockworkjava/kursspring/domain/Tournament.java
+++ src/main/java/com/clockworkjava/kursspring/domain/Tournament.java
@@ -4,30 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 public class Tournament {
 
     @Autowired
-    @Qualifier(value = "percival")
-    Knight knight;
-
+//    @Qualifier(value = "percival")
+    Set<Knight> knights;
     public Tournament() {
 
     }
 
     public void duel() {
-        knight.setAge(knight.getAge()+1);
+//        knight.setAge(knight.getAge()+1);
 
     }
 
-    public void setKnight(Knight knight) {
-        this.knight = knight;
+    public void setKnight(Set<Knight> knights) {
+        this.knights = knights;
     }
 
     @Override
     public String toString() {
 
-        return "W turnieju bierze udzial rycerz " + knight;
+        return "W turnieju biara  udzial rycerze " + knights.stream().map(Objects::toString).collect(Collectors.joining(","));
     }
 
 
