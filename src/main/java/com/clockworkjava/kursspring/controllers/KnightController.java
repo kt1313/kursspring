1e7924285257751effde591358ee7f569ce24d5e
kt1313 <tomaklim@wp.pl>
2021-08-26 04:21:48 +0200

    Lekcja 33 - Walidacja Formularzy, zakresy danych od uzytkownika (nie dziala!!! przeskakuje if'a z bindingResult.hasErrors() !!! do spr)

--- src/main/java/com/clockworkjava/kursspring/controllers/KnightController.java
+++ src/main/java/com/clockworkjava/kursspring/controllers/KnightController.java
@@ -1,18 +1,108 @@
+//package com.clockworkjava.kursspring.controllers;
+//
+//import com.clockworkjava.kursspring.components.TimeComponent;
+//import com.clockworkjava.kursspring.domain.Knight;
+//import com.clockworkjava.kursspring.domain.PlayerInformation;
+//import com.clockworkjava.kursspring.services.KnightService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import javax.validation.Valid;
+//import javax.websocket.server.PathParam;
+//import java.util.List;
+//
+//@Controller
+//public class KnightController {
+//
+//    @Autowired
+//    TimeComponent timeComponent;
+//
+//@Autowired
+//    PlayerInformation playerInformation;
+//
+//
+//
+//    @Autowired
+//    KnightService service;
+//
+//    @RequestMapping("/knights")
+//    public String getKnight(Model model) {
+//        List<Knight> allKnights = service.getAllKnights();
+//        model.addAttribute("knights", allKnights);
+//        model.addAttribute("timecomponent", timeComponent);
+//        model.addAttribute("playerinformation", playerInformation);
+//
+//
+//        return "knights";
+//    }
+//
+//    @RequestMapping("/knight")
+//    public String getKnight(@RequestParam("id") Integer id, Model model){
+//        Knight knight= service.getKnight(id);
+//   model.addAttribute("knight", knight);
+//        model.addAttribute("timecomponent", timeComponent);
+//        model.addAttribute("playerinformation", playerInformation);
+//
+//
+//        return "knight";
+//    }
+//
+//
+//    @RequestMapping("/newknight")
+//    public String createKnight(Model model){
+//    model.addAttribute("knight", new Knight());
+//        model.addAttribute("timecomponent", timeComponent);
+//        model.addAttribute("playerinformation", playerInformation);
+//
+//        return "knightform";
+//    }
+//
+//    @RequestMapping(value="/knights", method = RequestMethod.POST)
+//    public String saveKnight(@Valid Knight knight, BindingResult bindingResult) {
+//
+//        if(bindingResult.hasErrors()){
+//            System.out.println("There were errors");
+//    bindingResult.getAllErrors().forEach(error -> {
+//        System.out.println(error.getObjectName() + " " + error.getDefaultMessage());
+//    });
+//
+//            return "knightform";
+//        }else {
+//            service.saveKnight(knight);
+//            return "redirect:/knights";
+//        }
+//    }
+//
+//    @RequestMapping(value="/knight/delete/{id}")
+//    public String deleteKnight(@PathVariable("id") Integer id) {
+//        service.deleteKnight(id);
+//        return "redirect:/knights";
+//    }
+//}
 package com.clockworkjava.kursspring.controllers;
 
 import com.clockworkjava.kursspring.components.TimeComponent;
 import com.clockworkjava.kursspring.domain.Knight;
 import com.clockworkjava.kursspring.domain.PlayerInformation;
+import com.clockworkjava.kursspring.domain.repository.KnightRepository;
 import com.clockworkjava.kursspring.services.KnightService;
+//import com.sun.corba.se.spi.legacy.interceptor.RequestInfoExt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.websocket.server.PathParam;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -21,53 +111,57 @@ public class KnightController {
     @Autowired
     TimeComponent timeComponent;
 
-@Autowired
+    @Autowired
     PlayerInformation playerInformation;
-
-
 
     @Autowired
     KnightService service;
 
     @RequestMapping("/knights")
-    public String getKnight(Model model) {
+    public String getKnights(Model model) {
         List<Knight> allKnights = service.getAllKnights();
         model.addAttribute("knights", allKnights);
         model.addAttribute("timecomponent", timeComponent);
         model.addAttribute("playerinformation", playerInformation);
-
-
         return "knights";
     }
 
     @RequestMapping("/knight")
-    public String getKnight(@RequestParam("id") Integer id, Model model){
-        Knight knight= service.getKnight(id);
-   model.addAttribute("knight", knight);
+    public String getKnight(@RequestParam("id") Integer id, Model model) {
+        Knight knight = service.getKnight(id);
+        model.addAttribute("knight", knight);
         model.addAttribute("timecomponent", timeComponent);
         model.addAttribute("playerinformation", playerInformation);
-
-
         return "knight";
     }
 
-
     @RequestMapping("/newknight")
-    public String createKnight(Model model){
-    model.addAttribute("knight", new Knight());
+    public String createKnight(Model model) {
+        model.addAttribute("knight", new Knight());
         model.addAttribute("timecomponent", timeComponent);
         model.addAttribute("playerinformation", playerInformation);
-
         return "knightform";
     }
 
-    @RequestMapping(value="/knights", method = RequestMethod.POST)
-    public String saveKnight(Knight knight) {
-       service.saveKnight(knight);
-       return "redirect:/knights";
+    @RequestMapping(value = "/knights", method = RequestMethod.POST)
+    public String saveKnight(@Valid Knight knight, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("There were errors");
+            bindingResult.getAllErrors().forEach(error -> {
+                        System.out.println(error.getObjectName() + " " + error.getDefaultMessage());
+                    }
+            );
+            return "knightform";
+        } else {
+            service.saveKnight(knight);
+            System.out.println("metoda saveKnight");
+            return "redirect:/knights";
+        }
+
     }
 
-    @RequestMapping(value="/knight/delete/{id}")
+    @RequestMapping(value = "/knight/delete/{id}")
     public String deleteKnight(@PathVariable("id") Integer id) {
         service.deleteKnight(id);
         return "redirect:/knights";
--- src/main/java/com/clockworkjava/kursspring/domain/Knight.java
+++ src/main/java/com/clockworkjava/kursspring/domain/Knight.java
@@ -1,28 +1,133 @@
+//package com.clockworkjava.kursspring.domain;
+//
+//import javax.validation.constraints.Max;
+//import javax.validation.constraints.Min;
+////import org.hibernate.validator.constraints.Range;
+//import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
+//import java.util.Objects;
+//
+//public class Knight {
+//
+//    private int id;
+//    @NotNull
+//@Size(min = 2, max = 40, message = "Imie musi zawierac min2 i max 40 znakow")
+//    private String name;
+//
+//    @NotNull
+//
+//    @Min(value = 18, message = "Wiek musi byc wiekszy od 18 lat")
+//    @Max(value = 60, message = "Wiek musi byc mniejszy od 60 lat")
+//    private int age;
+//    private int level = 1;
+//
+//    private Quest quest;
+//
+//
+//    public Knight() {
+//
+//    }
+//
+//    public Knight(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//        this.level = 1;
+//
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Knight knight = (Knight) o;
+//        return age == knight.age;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(age);
+//    }
+//
+//    public void setQuest(Quest quest) {
+//        System.out.println("Ustawiam zadanie dla rycerza.");
+//        this.quest = quest;
+//    }
+//
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public int getAge() {
+//        return this.age;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public int getLevel() {
+//        return level;
+//    }
+//
+//    public void setLevel(int level) {
+//        this.level = level;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Rycerz o imieniu " + name + "(" + age + "). Ma za zadanie: " + quest;
+//    }
+//}
 package com.clockworkjava.kursspring.domain;
 
+//import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Knight {
 
     private int id;
 
+    @NotNull
+    @Size(min=2, max=40,message = "Imie rycerza musi miec miedzy 2, a 40 znakow")
     private String name;
 
+    @NotNull
+    @Min(value = 18, message = "Wiek musi byc 18><60")
+    @Max(value = 60, message = "Wiek musi byc 18><60")
+//    @Range(min=18, max=60, message = "Rycerz musi miec conajmniej 18 lat, a nie wiecej niz 60")
     private int age;
-    private int level=1;
+
+
+    private int level;
 
     private Quest quest;
 
-
     public Knight() {
-
+        this.level = 1;
     }
 
     public Knight(String name, int age) {
         this.name = name;
         this.age = age;
-        this.level=1;
-
+        this.level = 1;
     }
 
     @Override
@@ -43,6 +148,13 @@ public class Knight {
         this.quest = quest;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
 
     public String getName() {
         return name;
@@ -50,14 +162,6 @@ public class Knight {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return this.age;
     }
 
     public int getId() {
