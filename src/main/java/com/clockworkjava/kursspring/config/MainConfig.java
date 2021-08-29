55d0b3122ebdc1578020a4f4b40bcacd1b64f500
kt1313 <tomaklim@wp.pl>
2021-08-29 03:45:04 +0200

    Lekcja 43 - Security, dodanie uzytkownika i hasla. nie dziala, nie zaciaga zaleznosci do springboot-security i nie wczytuje klas..... port is outside of range....

--- src/main/java/com/clockworkjava/kursspring/config/MainConfig.java
+++ src/main/java/com/clockworkjava/kursspring/config/MainConfig.java
@@ -3,6 +3,7 @@ package com.clockworkjava.kursspring.config;
 import com.clockworkjava.kursspring.domain.repository.DBKnightRepository;
 import com.clockworkjava.kursspring.domain.repository.InMemoryRepository;
 import com.clockworkjava.kursspring.domain.repository.KnightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 
 @Configuration
@@ -23,4 +24,11 @@ public class MainConfig {
         return repo;
     }
 
+@Autowired
+    public void securityUsers(AuthenticationManagerBuilder auth){
+        auth.inMemoryAuthentication().
+                withUser("user1").password("user1").role("USER")
+                .and()
+                .withUser("user2").password("user2").role("ADMIN");
+}
 }
